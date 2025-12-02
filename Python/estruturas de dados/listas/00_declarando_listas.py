9d91cbb5a626c63eff9894e46a656efabb009fd5
Luiz Eduardo Vieira <duullivee@gmail.com>
2025-12-02 02:18:25 +0000

    Declarand uma Lista

---
+++ Python/estruturas de dados/listas/00_declarando_listas.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = [] #declara lista vazia
+print(frutas)
+
+letras = list("python") #o construtor "list" pede argumento iteravel
+print(letras)
+
+numeros = list(range(10)) #aqui cria um elemento para cada função range
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
